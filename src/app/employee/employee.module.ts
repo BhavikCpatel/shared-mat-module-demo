100305055ac3c88534bf6e5228c6bf230a1a5eab
Bhavik <bhavikcpatel@hotmail.com>
2020-03-28 18:10:14 -0400

    without shared module

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
-import { MaterialModule } from '../shared/material.module';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [EmployeeComponent],
-  imports: [CommonModule, EmployeeRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    EmployeeRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   exports: [EmployeeComponent]
 })
 export class EmployeeModule {}
